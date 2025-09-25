e0b8664ebee5b671ccb7481bdebb23bd598977d3
ali-haider-dev <alihaider061297@gmail.com>
2025-09-24 23:36:45 -0700

    careers page completed

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,10 +1,12 @@
 import CareersSection from '../..//sections/CareersSection'
 import React from 'react'
+import OpenPositions from './OpenPosition'
 
 const CareersPage = () => {
   return (
     <>
     <CareersSection/>
+    <OpenPositions/>
     </>
   )
 }
